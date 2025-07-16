c8f4c6361bbfcdc9cddae9afe481f2554f189bb7
daniel.ribas <daniel.ribas@isdbflowtech.com>
2025-07-16 16:10:02 -0300

    feat(form): início de construção do formulário

--- src/app/(auth)/finances/page.tsx
+++ src/app/(auth)/finances/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import FormFinances from "@/components/finances/form"
 import List from "@/components/finances/list"
 import Content from "@/components/templates/content"
 import SectionPage from "@/components/templates/section-page"
@@ -10,8 +11,9 @@ export default function Finances() {
 	const [transations, setTransation] = useState<Transation[]>(transationsFake)
 	return (
 		<SectionPage>
-			<Content>
+			<Content className="flex flex-col gap-5 justify-center">
 				<List transations={transations} />
+				<FormFinances transation={transations[0]}/>
 			</Content>
 		</SectionPage>
 	)
--- src/components/finances/list.tsx
+++ src/components/finances/list.tsx
@@ -1,6 +1,7 @@
 import Transation from "@/logic/core/finances/Transation"
 import DateFormat from "@/logic/utils/Data"
 import Money from "@/logic/utils/Money"
+import { IconTrendingDown, IconTrendingUp } from "@tabler/icons-react"
 
 interface ListProps {
 	transations: Transation[]
@@ -15,6 +16,7 @@ export default function List({ transations }: ListProps) {
 						key={t.id}
 						className={`flex items-center gap-3 p-3 cursor-pointer rounded-lg ${i % 2 === 0 ? "bg-logo-purple" : "bg-logo-purple/70"} hover:bg-zinc-300/50`}
 					>
+						<span className={`flex justify-center items-center w-8 h-8 p-1.5 rounded-full ${t.tipo === 'receita' ? 'bg-green-500' : 'bg-red-500'}`}>{t.tipo === 'receita' ? <IconTrendingUp/> : <IconTrendingDown/>}</span>
 						<span className="w-full md:w-1/2">{t.descricao}</span>
 						<span className="hidden md:inline flex-1">{DateFormat.ddmmyy.format(t.data)}</span>
 						<span>{Money.formatMoney(t.valor)}</span>
