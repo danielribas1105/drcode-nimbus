715a5e925e9518d39a8cc975f1042fde83a5748e
Daniel <danielribas1105@gmail.com>
2025-07-16 18:20:26 -0300

    feat(form): continuação formulário

--- src/components/finances/form.tsx
+++ src/components/finances/form.tsx
@@ -1,34 +1,44 @@
-import Transation from "@/logic/core/finances/Transation";
-import { TransationType } from "@/logic/core/finances/TransationType";
-import Money from "@/logic/utils/Money";
-import { TextInput, Radio, Group } from "@mantine/core";
-import { DatePickerInput } from "@mantine/dates";
-import "dayjs/locale/pt-br";
+import Transation from "@/logic/core/finances/Transation"
+import { TransationType } from "@/logic/core/finances/TransationType"
+import Money from "@/logic/utils/Money"
+import { TextInput, Radio, Group, Button } from "@mantine/core"
+import { DatePickerInput } from "@mantine/dates"
+import "dayjs/locale/pt-br"
 
 interface FormFinancesProps {
-   transation: Transation
+	transation: Transation
 }
 
-export default function FormFinances({transation}: FormFinancesProps) {
+export default function FormFinances({ transation }: FormFinancesProps) {
+	function changeAttribute(type: string) {}
 
-   function changeAttribute(type: string) {
-
-   }
-
-   return (
-      <div className="flex flex-col border border-zinc-500 rounded-xl overflow-hidden">
-         <div className="bg-white/60 py-3 px-7 text-zinc-800">{transation.descricao}</div>
-         <div className="flex flex-col gap-4 p-4 sm:p-7">
-            <TextInput label="Descrição" value={transation.descricao}/>
-            <TextInput label="Valor" value={Money.formatMoney(transation.valor)}/>
-            <DatePickerInput label="Data" value={transation.data} locale="pt-BR" valueFormat="DD/MM/YYYY"/>
-            <Radio.Group value={transation.tipo} onChange={() => changeAttribute('tipo')}>
-               <Group>
-                  <Radio value={TransationType.RECEITA} label="Receita"/>
-                  <Radio value={TransationType.DESPESA} label="Despesa"/>
-               </Group>
-            </Radio.Group>
-         </div>
-      </div>
-   )
+	return (
+		<div className="flex flex-col border border-zinc-500 bg-purple-900/50 rounded-xl overflow-hidden">
+			<div className="bg-white/60 py-3 px-7 text-zinc-800">{transation.descricao}</div>
+			<div className="flex flex-col gap-4 p-4 sm:p-7">
+				<TextInput label="Descrição" value={transation.descricao} />
+				<TextInput label="Valor" value={Money.formatMoney(transation.valor)} />
+				<DatePickerInput label="Data" value={transation.data} locale="pt-BR" valueFormat="DD/MM/YYYY" />
+				<Radio.Group value={transation.tipo} onChange={() => changeAttribute("tipo")}>
+					<Group>
+						<Radio value={TransationType.RECEITA} label="Receita" />
+						<Radio value={TransationType.DESPESA} label="Despesa" />
+					</Group>
+				</Radio.Group>
+				<div className="flex gap-5">
+					<Button className="bg-green-500" color="green">
+						Salvar
+					</Button>
+					<Button className="bg-zinc-500" color="gray">
+						Voltar
+					</Button>
+					{transation.id && (
+						<Button className="bg-red-500" color="red">
+							Excluir
+						</Button>
+					)}
+				</div>
+			</div>
+		</div>
+	)
 }
--- src/components/finances/list.tsx
+++ src/components/finances/list.tsx
@@ -5,9 +5,10 @@ import { IconTrendingDown, IconTrendingUp } from "@tabler/icons-react"
 
 interface ListProps {
 	transations: Transation[]
+	selectTransation?: (transation: Transation) => void
 }
 
-export default function List({ transations }: ListProps) {
+export default function List({ transations, selectTransation }: ListProps) {
 	return (
 		<div className="flex flex-col gap-3">
 			{transations.length > 0 ? (
@@ -15,8 +16,13 @@ export default function List({ transations }: ListProps) {
 					<div
 						key={t.id}
 						className={`flex items-center gap-3 p-3 cursor-pointer rounded-lg ${i % 2 === 0 ? "bg-logo-purple" : "bg-logo-purple/70"} hover:bg-zinc-300/50`}
+						onClick={() => selectTransation?.(t)}
 					>
-						<span className={`flex justify-center items-center w-8 h-8 p-1.5 rounded-full ${t.tipo === 'receita' ? 'bg-green-500' : 'bg-red-500'}`}>{t.tipo === 'receita' ? <IconTrendingUp/> : <IconTrendingDown/>}</span>
+						<span
+							className={`flex justify-center items-center w-8 h-8 p-1.5 rounded-full ${t.tipo === "receita" ? "bg-green-500" : "bg-red-500"}`}
+						>
+							{t.tipo === "receita" ? <IconTrendingUp /> : <IconTrendingDown />}
+						</span>
 						<span className="w-full md:w-1/2">{t.descricao}</span>
 						<span className="hidden md:inline flex-1">{DateFormat.ddmmyy.format(t.data)}</span>
 						<span>{Money.formatMoney(t.valor)}</span>
